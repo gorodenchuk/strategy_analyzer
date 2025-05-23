cbd1ce1dbce1b84d48e58340ce18ec4944e28202
NazarGorodenchuk <gorodenchuk.n@gmail.com>
2025-05-23 13:52:15 +0300

    added test

--- src/main/java/stat/EntryModelsStatistic.java
+++ src/main/java/stat/EntryModelsStatistic.java
@@ -66,29 +66,39 @@ public class EntryModelsStatistic extends BaseStatistic {
 
     public void smrLowFractalTarget(double risk, List<Candle> sweepLiquidityCandles, List<Candle> fractalsHigh, List<Candle> candlesOnMinus1TF, List<Candle> fractalsLowMinus1TF, List<Candle> candlesOnMinus2TF, List<Candle> fractalsLowMinus2TF, List<Candle> fractalsHighMinus2TF) {
         for (Candle candleParent : sweepLiquidityCandles) {
-            Candle candleInvlMinus1TF = candleHelper.getCandleByHigh(candlesOnMinus1TF, candleParent);
-            int index = candleHelper.getLowFractalIndexBeforeSweep(fractalsLowMinus1TF, candleInvlMinus1TF);
+            Candle candleInvlMinus1TF = candleHelper.getCandleByHigh(candlesOnMinus1TF,
+                candleParent);
+            int index = candleHelper.getLowFractalIndexBeforeSweep(fractalsLowMinus1TF,
+                candleInvlMinus1TF);
 
             if (index != -1) {
                 /*
                  take profit is higher than first fractal on -1TF after sweep. Could be changed in the future.
                 */
                 Candle takeProfitMinus1TF = fractalsLowMinus1TF.get(index);
-                Candle takeProfitMinus2TF = candleHelper.getCandleByLow(candlesOnMinus2TF, takeProfitMinus1TF);
-                Candle candleInvlMinus2TF = candleHelper.getCandleByHigh(candlesOnMinus2TF, candleInvlMinus1TF);
+                Candle takeProfitMinus2TF = candleHelper.getCandleByLow(candlesOnMinus2TF,
+                    takeProfitMinus1TF);
+                Candle candleInvlMinus2TF = candleHelper.getCandleByHigh(candlesOnMinus2TF,
+                    candleInvlMinus1TF);
 
-                Candle rebalancedFractalMinus2TF = candleHelper.getHighRebalancedFractalMinus2TF(candlesOnMinus2TF, fractalsHigh, candleInvlMinus1TF);
-                Candle smrVlCandle = candleHelper.getTargetLowLevelValidation(candlesOnMinus2TF, fractalsLowMinus2TF, candleInvlMinus2TF);
+                Candle rebalancedFractalMinus2TF = candleHelper.getHighRebalancedFractalMinus2TF(
+                    candlesOnMinus2TF, fractalsHigh, candleInvlMinus1TF);
+                Candle smrVlCandle = candleHelper.getTargetLowLevelValidation(candlesOnMinus2TF,
+                    fractalsLowMinus2TF, candleInvlMinus2TF);
 
-                double entry = Double.parseDouble(entryPoint.getBosLowLevel(fractalsLowMinus2TF, candleInvlMinus2TF).getMid().getL());
+                double entry = Double.parseDouble(
+                    entryPoint.getBosLowLevel(fractalsLowMinus2TF, candleInvlMinus2TF).getMid()
+                        .getL());
                 double takeProfit = Double.parseDouble(takeProfitMinus2TF.getMid().getL());
                 double stopLoss = Double.parseDouble(candleInvlMinus2TF.getMid().getH());
 
-                smr = new Smr(fractalsLowMinus2TF, fractalsHighMinus2TF, candlesOnMinus2TF, candleInvlMinus2TF, rebalancedFractalMinus2TF, smrVlCandle);
+                smr = new Smr(fractalsLowMinus2TF, fractalsHighMinus2TF, candlesOnMinus2TF,
+                    candleInvlMinus2TF, rebalancedFractalMinus2TF, smrVlCandle);
                 boolean isSnr = smr.isSmrShort();
                 boolean isSnrEntry;
 
-                String targetResult = target.getReachedLowFractalTarget(candlesOnMinus2TF, takeProfitMinus2TF, candleInvlMinus2TF);
+                String targetResult = target.getReachedLowFractalTarget(candlesOnMinus2TF,
+                    takeProfitMinus2TF, candleInvlMinus2TF);
 
                 if (isSnr) {
                     isSnrEntry = smr.entryModelShortTest();
@@ -100,13 +110,119 @@ public class EntryModelsStatistic extends BaseStatistic {
                         rr += riskRewards.getRiskReward(targetResult);
                     }
 
-                    if (targetResult.equals("TP")) wins++;
-                    else if (targetResult.equals("SL")) losses++;
-                    else be++;
+                    if (targetResult.equals("TP"))
+                        wins++;
+                    else if (targetResult.equals("SL"))
+                        losses++;
+                    else
+                        be++;
+                }
+            }
+        }
+    }
+
+        public void smrHighFractalTarget(double risk, List<Candle> sweepLiquidityCandles, List<Candle> fractalsLow, List<Candle> fractalsHigh, List<Candle> candlesOnMinus1TF, List<Candle> fractalsHighMinus1TF, List<Candle> candlesOnMinus2TF, List<Candle> fractalsHighMinus2TF, List<Candle> fractalsLowMinus2TF) {
+
+            for (Candle candleParent : sweepLiquidityCandles) {
+                Candle candleInvlMinus1TF = candleHelper.getCandleByLow(candlesOnMinus1TF, candleParent);
+                int index = candleHelper.getHighFractalIndexBeforeSweep(fractalsHigh, candleParent);
+
+                if (index != -1) {
+                /*
+                 take profit is higher than the first fractal on -1TF after sweep. Could be changed in the future.
+                */
+                    Candle takeProfitContextTF = fractalsHigh.get(index);
+                    Candle takeProfitMinus1TF = candleHelper.getCandleByHigh(candlesOnMinus1TF, takeProfitContextTF);
+                    Candle takeProfitMinus2TF = candleHelper.getCandleByHigh(candlesOnMinus2TF, takeProfitMinus1TF);
+                    Candle candleInvlMinus2TF = candleHelper.getCandleByLow(candlesOnMinus2TF, candleInvlMinus1TF);
+
+                    Candle rebalancedFractalMinus2TF = candleHelper.getLowRebalancedFractalMinus2TF(candlesOnMinus2TF, fractalsLow, candleInvlMinus1TF);
+                    Candle smrVlCandle = candleHelper.getTargetHighLevelValidation(candlesOnMinus2TF, fractalsHighMinus2TF, candleInvlMinus2TF);
+
+                    double entry = Double.parseDouble(entryPoint.getBosHighLevel(fractalsHighMinus2TF, candleInvlMinus2TF).getMid().getH());
+                    double takeProfit = Double.parseDouble(takeProfitMinus2TF.getMid().getH());
+                    double stopLoss = Double.parseDouble(candleInvlMinus2TF.getMid().getL());
+
+                    smr = new Smr(fractalsLowMinus2TF, fractalsHighMinus2TF, candlesOnMinus2TF, candleInvlMinus2TF, rebalancedFractalMinus2TF, smrVlCandle);
+                    boolean isSnr = smr.isSmrLong();
+                    boolean isSnrEntry;
+
+                    String targetResult = target.getReachedHighFractalTarget(candlesOnMinus2TF, takeProfitMinus2TF, candleInvlMinus2TF);
+
+                    if (isSnr) {
+                        isSnrEntry = smr.entryModelLongTest();
+                        entryModel++;
+
+                        if (isSnrEntry) {
+                            riskRewards = new RiskRewards(entry, takeProfit, stopLoss, risk);
+                            entryModelTest++;
+                            rr += riskRewards.getRiskReward(targetResult);
+                        }
+
+                        if (targetResult.equals("TP")) wins++;
+                        else if (targetResult.equals("SL")) losses++;
+                        else be++;
+                    }
+                }
+            }
+        }
+
+        public void smrLowFractalTarget(double risk, List<Candle> sweepLiquidityCandles, List<Candle> fractalsHigh, List<Candle> fractalsLow, List<Candle> candlesOnMinus1TF, List<Candle> fractalsLowMinus1TF, List<Candle> candlesOnMinus2TF, List<Candle> fractalsLowMinus2TF, List<Candle> fractalsHighMinus2TF) {
+            for (Candle candleParent : sweepLiquidityCandles) {
+                Candle candleInvlMinus1TF = candleHelper.getCandleByHigh(candlesOnMinus1TF,
+                    candleParent);
+                int index = candleHelper.getLowFractalIndexBeforeSweep(fractalsLow, candleParent);
+
+                if (index != -1) {
+                /*
+                 take profit is higher than first fractal on -1TF after sweep. Could be changed in the future.
+                */
+                    Candle takeProfitContextTF = fractalsLow.get(index);
+                    Candle takeProfitMinus1TF = candleHelper.getCandleByLow(candlesOnMinus1TF, takeProfitContextTF);
+                    Candle takeProfitMinus2TF = candleHelper.getCandleByLow(candlesOnMinus2TF,
+                        takeProfitMinus1TF);
+                    Candle candleInvlMinus2TF = candleHelper.getCandleByHigh(candlesOnMinus2TF,
+                        candleInvlMinus1TF);
+
+                    Candle rebalancedFractalMinus2TF = candleHelper.getHighRebalancedFractalMinus2TF(
+                        candlesOnMinus2TF, fractalsHigh, candleInvlMinus1TF);
+                    Candle smrVlCandle = candleHelper.getTargetLowLevelValidation(candlesOnMinus2TF,
+                        fractalsLowMinus2TF, candleInvlMinus2TF);
+
+                    double entry = Double.parseDouble(
+                        entryPoint.getBosLowLevel(fractalsLowMinus2TF, candleInvlMinus2TF).getMid()
+                            .getL());
+                    double takeProfit = Double.parseDouble(takeProfitMinus2TF.getMid().getL());
+                    double stopLoss = Double.parseDouble(candleInvlMinus2TF.getMid().getH());
+
+                    smr = new Smr(fractalsLowMinus2TF, fractalsHighMinus2TF, candlesOnMinus2TF,
+                        candleInvlMinus2TF, rebalancedFractalMinus2TF, smrVlCandle);
+                    boolean isSnr = smr.isSmrShort();
+                    boolean isSnrEntry;
+
+                    String targetResult = target.getReachedLowFractalTarget(candlesOnMinus2TF,
+                        takeProfitMinus2TF, candleInvlMinus2TF);
+
+                    if (isSnr) {
+                        isSnrEntry = smr.entryModelShortTest();
+                        entryModel++;
+
+                        if (isSnrEntry) {
+                            riskRewards = new RiskRewards(entry, takeProfit, stopLoss, risk);
+                            entryModelTest++;
+                            rr += riskRewards.getRiskReward(targetResult);
+                        }
+
+                        if (targetResult.equals("TP"))
+                            wins++;
+                        else if (targetResult.equals("SL"))
+                            losses++;
+                        else
+                            be++;
+                    }
                 }
             }
         }
 
 
-    }
 }
